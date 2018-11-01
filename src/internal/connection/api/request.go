6d29554bb2049e796fa4753a8bdf48a339575b69
Nickolas Gough <nickolas.v.gough@gmail.com>
2018-11-01 14:00:16 -0600

    Create connection requests.

---
+++ src/internal/connection/api/request.go
@@ -0,0 +1,67 @@
+package api
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"social-cloud-server/src/server/endpoint"
+	"social-cloud-server/src/database"
+)
+
+type RequestHandler struct {
+	db *database.Database
+}
+
+func NewRequestHandler(db *database.Database) *RequestHandler {
+	return &RequestHandler{
+		db: db,
+	}
+}
+
+type RequestRequest struct {
+	Username   string    `json:"username"`
+	Connection string    `json:"post"`
+	Datetime   time.Time `json:"datetime"`
+}
+
+type RequestResponse struct {
+	Success bool `json:"success"`
+}
+
+func (c *RequestHandler) Request() endpoint.Request {
+	return &RequestRequest{}
+}
+
+func (c *RequestHandler) Process(ctx context.Context, request endpoint.Request) (endpoint.Response, error) {
+	cr, ok := request.(*RequestRequest)
+	if !ok {
+		return nil, errors.New("error: received a request that is not a RequestRequest")
+	}
+
+	err := c.db.ExecQuery(c.db.BuildQuery(requestQuery, cr.Username, cr.Connection, cr.Datetime.Format(time.RFC3339)))
+	if err != nil {
+		return &RequestResponse{
+			Success: false,
+		}, err
+	}
+
+	return &RequestResponse{
+		Success: true,
+	}, nil
+}
+
+const requestQuery = `
+INSERT INTO notification (
+	username,
+	type,
+	sender,
+	datetime
+)
+VALUES (
+	'%s',
+	'connection-request',
+	'%s',
+	'%s'
+);
+`
--- src/server/routes.go
+++ src/server/routes.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	profileApi "social-cloud-server/src/internal/profile/api"
+	connectionApi "social-cloud-server/src/internal/connection/api"
 	postApi "social-cloud-server/src/internal/post/api"
 
 	"social-cloud-server/src/server/endpoint"
@@ -10,6 +11,7 @@ import (
 func (s *Server) Routes() map[string]endpoint.Handler {
 	return map[string]endpoint.Handler{
 		"/profile/create": profileApi.NewCreateHandler(s.Database),
+		"/connection/request": connectionApi.NewRequestHandler(s.Database),
 		"/post/create": postApi.NewCreateHandler(s.Database),
 	}
 }
