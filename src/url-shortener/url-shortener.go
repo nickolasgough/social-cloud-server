407ee82e55d9b9b655f29d8c01a7d535948ca3e7
Nickolas Gough <nickolas.v.gough@gmail.com>
2018-11-23 14:54:50 -0600

    Change to using bitly.

--- src/url-shortener/url-shortener.go
+++ src/url-shortener/url-shortener.go
@@ -9,8 +9,8 @@ import (
 )
 
 const (
-	apiKey = "AIzaSyCrRYQY0HKi5B0ANQFcPmNJbYAY6Pb6aYs"
-	apiUrl = "https://www.googleapis.com/urlshortener/v1/url"
+	apiKey = "2e46a37bf73623ba49a12ecec034561470943189"
+	apiUrl = "https://api-ssl.bitly.com/v4/bitlinks"
 )
 
 type apiRequest struct {
@@ -32,11 +32,15 @@ func ShortenUrl(longUrl string) (string, error) {
 		return "", err
 	}
 
-	requestUrl := fmt.Sprintf("%s?key=%s", apiUrl, apiKey)
-	fmt.Printf("Request URL: %s\n", requestUrl)
-	fmt.Printf("Request body: %s\n", requestBody)
-	bodyReader := bytes.NewReader(requestBody)
-	rawResponse, err := http.Post(requestUrl, "application/json", bodyReader)
+	httpClient := &http.Client{}
+	requestReader := bytes.NewReader(requestBody)
+	postRequest, err := http.NewRequest("POST", apiUrl, requestReader)
+	if err != nil {
+		return "", err
+	}
+	postRequest.Header.Add("Content-Type", "application/json")
+	postRequest.Header.Add("Authorization", fmt.Sprintf("Bearer %s", apiKey))
+	rawResponse, err := httpClient.Do(postRequest)
 	if err != nil {
 		return "", err
 	}
